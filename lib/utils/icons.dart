7674c8a21d987c26b6537ad41b165ceeaedc35bc
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-07-23 21:49:44 +0300

    Adding svgs, fonts, colors, and making classes

---
+++ lib/utils/icons.dart
@@ -0,0 +1,21 @@
+class MyIcons {
+  static const archive = 'assets/svgs/archive_icon.svg';
+
+  static const call = 'assets/svgs/call_icon.svg';
+
+  static const chat = 'assets/svgs/chat_icon.svg';
+
+  static const group = 'assets/svgs/group_icon.svg';
+
+  static const profile = 'assets/svgs/profile_icon.svg';
+
+  static const settings = 'assets/svgs/settings_icon.svg';
+
+  static const share = 'assets/svgs/share_icon.svg';
+
+  static const status = 'assets/svgs/status_icon.svg';
+  
+  static const video = 'assets/svgs/video_icon.svg';
+  
+  //static const none = 'assets/svgs/.svg';
+}
---
+++ lib/utils/images.dart
@@ -0,0 +1,19 @@
+class MyImages {
+  static const blackGirl = 'assets/svgs/black_girl_taking_selfie.svg';
+
+  static const computer =
+      'assets/svgs/Computer_with_the_checkmark_confirming_the_user_is_logged_into_account.svg';
+
+  static const dialogue = 'assets/svgs/dialogue.svg';
+
+  static const firendsVideoCall = 'assets/svgs/Friends_talking_on_video_call.svg';
+
+  static const messaging = 'assets/svgs/Messaging.svg';
+
+  static const smartPhonePassword = 'assets/svgs/Smartphone_password_protection_and_cybersecurity.svg';
+  
+  static const youngManTexting = 'assets/svgs/young_man_texting_his_significant_other.svg';
+  
+  static const voiceMessage = 'assets/svgs/voice_message.svg';
+  //static const none = 'assets/svgs/.svg';
+}
