bdb5b48084acfc9e76b0af86649f546e237b1c19
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-07-24 23:41:41 +0300

    User Chats Screen Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:app_co/manager/getXControllers/services.dart';
+import 'package:app_co/routes.dart';
 import 'package:app_co/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'features/chat/user_chats_screen.dart';
+import 'package:get/get.dart';
 
-void main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await initServices();
   runApp(const MyApp());
@@ -14,47 +15,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: themeData,
-      home: const ChatScreen(),
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: MyColors.green),
+          useMaterial3: true,
+        fontFamily: "Inter"
+      ),
+      getPages: routes,
     );
   }
 }
-
-var themeData = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: MyColors.green),
-  useMaterial3: true,
-  fontFamily: "Inter",
-);
-
-// import 'package:app_co/manager/getXControllers/services.dart';
-// import 'package:app_co/routes.dart';
-// import 'package:app_co/utils/colors.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await initServices();
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: themeData,
-//       getPages: routes,
-//     );
-//   }
-// }
-//
-// var themeData = ThemeData(
-//   colorScheme: ColorScheme.fromSeed(seedColor: MyColors.green),
-//   useMaterial3: true,
-//   fontFamily: "Inter",
-// );
