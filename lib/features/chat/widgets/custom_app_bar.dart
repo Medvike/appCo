738fe2c4d0ab9430c3b75ba03c771a2faa86a949
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-07-24 23:33:11 +0300

    User Chats Screen Ui

---
+++ lib/features/chat/widgets/custom_app_bar.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:app_co/features/chat/widgets/stack_child_widget.dart';
+import '../../../utils/colors.dart';
+import '../../../utils/icons.dart';
+import '../../../utils/widgets/profile_picture_widget.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    Key? key,
+    required this.userImage,
+    required this.userName,
+  }) : super(key: key);
+
+  final String userImage,userName;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: width * .06, vertical: height * .04),
+      child: Row(
+        children: [
+          ProfilePictureWidget(imageUrl: userImage,size: .165),
+          SizedBox(width: width * .045),
+          Text(
+            userName,
+            style: const TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+          ),
+          const Spacer(),
+          SizedBox(
+            width: width * .4,
+            child: Stack(
+              alignment: Alignment.centerRight,
+              children: [
+                StackChildWidget(
+                  function: () {},
+                  icon: MyIcons.settings,
+                  color: MyColors.green,
+                ),
+                StackChildWidget(
+                  function: () {},
+                  icon: MyIcons.profile,
+                  color: Colors.black,
+                  position: 48,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:app_co/manager/getXControllers/services.dart';
-import 'package:app_co/routes.dart';
 import 'package:app_co/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'features/chat/user_chats_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initServices();
   runApp(const MyApp());
@@ -15,18 +14,47 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: MyColors.green),
-          useMaterial3: true,
-        fontFamily: "Inter"
-      ),
-      getPages: routes,
+      theme: themeData,
+      home: const ChatScreen(),
     );
   }
 }
 
+var themeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: MyColors.green),
+  useMaterial3: true,
+  fontFamily: "Inter",
+);
 
-// TODO: hi
-// TODO: hello
+// import 'package:app_co/manager/getXControllers/services.dart';
+// import 'package:app_co/routes.dart';
+// import 'package:app_co/utils/colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await initServices();
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: themeData,
+//       getPages: routes,
+//     );
+//   }
+// }
+//
+// var themeData = ThemeData(
+//   colorScheme: ColorScheme.fromSeed(seedColor: MyColors.green),
+//   useMaterial3: true,
+//   fontFamily: "Inter",
+// );
